045c6783480e2d2e9fdba42d1e1c420581051bb2
Nicole Oo <yxyohy@gmail.com>
2023-10-01 12:36:52 +0800

    Fixed problem with port_distance for more than 2 ports

--- server/helper_functions.py
+++ server/helper_functions.py
@@ -3,7 +3,7 @@ import port_distance_client
 def calculate_accumulated_distance(port_list):
     total_distance = 0
     for i in range(0,len(port_list)-2):
-        port_pair = sorted([ports[i], ports[i+1]])
+        port_pair = sorted([port_list[i], port_list[i+1]])
         distance = port_distance_client.get_route_distance(port_pair[0], port_pair[1])
         total_distance += distance
     return total_distance
